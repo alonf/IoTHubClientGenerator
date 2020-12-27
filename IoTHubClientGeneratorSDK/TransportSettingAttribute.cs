761de08841578dfecbf5876a9d6959bd279feec4
Alon Fliess <alonf@codevalue.net>
2020-12-27 23:15:00 +0200

    Update doc and remove generate package on build

--- IoTHubClientGeneratorSDK/TransportSettingAttribute.cs
+++ IoTHubClientGeneratorSDK/TransportSettingAttribute.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using Microsoft.Azure.Devices.Client;
 
 namespace IoTHubClientGeneratorSDK
 {
+    /// <summary>
+    /// Add a <see cref="ITransportSettings"/> instance to the Device Client creation
+    /// You can have more than one property decorated with this attribute
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class TransportSettingAttribute : Attribute
     {
